c3af9ae87343de8cfb416e5093f6af8ae54e853a
GCZ <1635320153@qq.com>
2020-06-25 16:45:25 +0800

    新建了GoodsService工具类

---
+++ src/main/java/edu/nwpu/market/service/NWPUMarketGoodsService.java
@@ -0,0 +1,11 @@
+package edu.nwpu.market.service;
+
+public interface NWPUMarketGoodsService {
+    /**
+     * 获取商品详情
+     *
+     * @param id
+     * @return
+     */
+    NWPUMarketGoodsService getNWPUMarketGoodsById(Long id);
+}
