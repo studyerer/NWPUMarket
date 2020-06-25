c0e6e3df454a43ef8259d54c150fe620491154fe
bianyan155 <2298117537@qq.com>
2020-06-25 22:00:06 +0800

    完成后台登录功能

---
+++ src/main/java/edu/nwpu/market/dao/AdminUserMapper.java
@@ -0,0 +1,20 @@
+package edu.nwpu.market.dao;
+
+import edu.nwpu.market.entity.AdminUser;
+import org.apache.ibatis.annotations.Param;
+
+public interface AdminUserMapper {
+    int insert(AdminUser record);
+
+    int insertSelective(AdminUser record);
+
+    /**
+     * 登陆方法
+     *
+     * @param userName
+     * @param password
+     * @return
+     */
+    AdminUser login(@Param("userName") String userName, @Param("password") String password);
+
+}
