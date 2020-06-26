6444e49ea52d7e55d2c19eb823be8db01316c2a6
freehreat <1765578099@qq.com>
2020-06-26 17:54:33 +0800

    更新dao层与购物车订单相关部分

---
+++ src/main/java/edu/nwpu/market/dao/NWPUMarketGoodsMapper.java
@@ -0,0 +1,41 @@
+package edu.nwpu.market.dao;
+
+import edu.nwpu.market.entity.NWPUMarketGoods;
+import edu.nwpu.market.entity.StockNumDTO;
+import edu.nwpu.market.util.PageQueryUtil;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface NWPUMarketGoodsMapper {
+    int deleteByPrimaryKey(Long goodsId);
+
+    int insert(NWPUMarketGoods record);
+
+    int insertSelective(NWPUMarketGoods record);
+
+    NWPUMarketGoods selectByPrimaryKey(Long goodsId);
+
+    int updateByPrimaryKeySelective(NWPUMarketGoods record);
+
+    int updateByPrimaryKeyWithBLOBs(NWPUMarketGoods record);
+
+    int updateByPrimaryKey(NWPUMarketGoods record);
+
+    List<NWPUMarketGoods> findNWPUMarketGoodsList(PageQueryUtil pageUtil);
+
+    int getTotalNWPUMarketGoods(PageQueryUtil pageUtil);
+
+    List<NWPUMarketGoods> selectByPrimaryKeys(List<Long> goodsIds);
+
+    List<NWPUMarketGoods> findNWPUMarketGoodsListBySearch(PageQueryUtil pageUtil);
+
+    int getTotalNWPUMarketGoodsBySearch(PageQueryUtil pageUtil);
+
+    int batchInsert(@Param("newBeeMallGoodsList") List<NWPUMarketGoods> newBeeMallGoodsList);
+
+    int updateStockNum(@Param("stockNumDTOS") List<StockNumDTO> stockNumDTOS);
+
+    int batchUpdateSellStatus(@Param("orderIds")Long[] orderIds,@Param("sellStatus") int sellStatus);
+
+}
