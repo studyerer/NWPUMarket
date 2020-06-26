04da79cb036a8c5599e20288a91785b192e1f971
bianyan155 <2298117537@qq.com>
2020-06-26 15:45:22 +0800

    默认的

---
+++ src/main/resources/static/admin/dist/js/public.js
@@ -0,0 +1,55 @@
+/**
+ * 获取jqGrid选中的一条记录
+ * @returns {*}
+ */
+function getSelectedRow() {
+    var grid = $("#jqGrid");
+    var rowKey = grid.getGridParam("selrow");
+    if (!rowKey) {
+        swal("请选择一条记录", {
+            icon: "warning",
+        });
+        return;
+    }
+    var selectedIDs = grid.getGridParam("selarrrow");
+    if (selectedIDs.length > 1) {
+        swal("只能选择一条记录", {
+            icon: "warning",
+        });
+        return;
+    }
+    return selectedIDs[0];
+}
+
+/**
+ * 获取jqGrid选中的一条记录(不出现弹框)
+ * @returns {*}
+ */
+function getSelectedRowWithoutAlert() {
+    var grid = $("#jqGrid");
+    var rowKey = grid.getGridParam("selrow");
+    if (!rowKey) {
+        return;
+    }
+    var selectedIDs = grid.getGridParam("selarrrow");
+    if (selectedIDs.length > 1) {
+        return;
+    }
+    return selectedIDs[0];
+}
+
+/**
+ * 获取jqGrid选中的多条记录
+ * @returns {*}
+ */
+function getSelectedRows() {
+    var grid = $("#jqGrid");
+    var rowKey = grid.getGridParam("selrow");
+    if (!rowKey) {
+        swal("请选择一条记录", {
+            icon: "warning",
+        });
+        return;
+    }
+    return grid.getGridParam("selarrrow");
+}
