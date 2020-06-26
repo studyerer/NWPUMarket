a9d84d300f1a14d413ba9269b643545dc4647019
vaeal <45780395+vaeal@users.noreply.github.com>
2020-06-26 19:15:21 +0800

    增加user的服务接口和实现

---
+++ src/main/java/edu/nwpu/market/service/impl/MallUserServiceImpl.java
@@ -0,0 +1,96 @@
+
+package edu.nwpu.market.service.impl;
+
+import edu.nwpu.market.common.Constants;
+import edu.nwpu.market.common.ServiceResultEnum;
+import edu.nwpu.market.controller.vo.MallUserVO;
+import edu.nwpu.market.dao.MallUserMapper;
+import edu.nwpu.market.entity.MallUser;
+import edu.nwpu.market.service.MallUserService;
+import edu.nwpu.market.util.BeanUtil;
+import edu.nwpu.market.util.MD5Util;
+import edu.nwpu.market.util.PageQueryUtil;
+import edu.nwpu.market.util.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Service
+public class MallUserServiceImpl implements MallUserService {
+
+    @Autowired
+    private MallUserMapper mallUserMapper;
+
+    @Override
+    public PageResult getMallUsersPage(PageQueryUtil pageUtil) {
+        List<MallUser> mallUsers = mallUserMapper.findMallUserList(pageUtil);
+        int total = mallUserMapper.getTotalMallUsers(pageUtil);
+        PageResult pageResult = new PageResult(mallUsers, total, pageUtil.getLimit(), pageUtil.getPage());
+        return pageResult;
+    }
+
+    @Override
+    public String register(String loginName, String password) {
+        if (mallUserMapper.selectByLoginName(loginName) != null) {
+            return ServiceResultEnum.SAME_LOGIN_NAME_EXIST.getResult();
+        }
+        MallUser registerUser = new MallUser();
+        registerUser.setLoginName(loginName);
+        registerUser.setNickName(loginName);
+        String passwordMD5 = MD5Util.MD5Encode(password, "UTF-8");
+        registerUser.setPasswordMd5(passwordMD5);
+        if (mallUserMapper.insertSelective(registerUser) > 0) {
+            return ServiceResultEnum.SUCCESS.getResult();
+        }
+        return ServiceResultEnum.DB_ERROR.getResult();
+    }
+
+    @Override
+    public String login(String loginName, String passwordMD5, HttpSession httpSession) {
+        MallUser user = mallUserMapper.selectByLoginNameAndPasswd(loginName, passwordMD5);
+        if (user != null && httpSession != null) {
+            if (user.getLockedFlag() == 1) {
+                return ServiceResultEnum.LOGIN_USER_LOCKED.getResult();
+            }
+            //昵称太长 影响页面展示
+            if (user.getNickName() != null && user.getNickName().length() > 7) {
+                String tempNickName = user.getNickName().substring(0, 7) + "..";
+                user.setNickName(tempNickName);
+            }
+            MallUserVO newBeeMallUserVO = new MallUserVO();
+            BeanUtil.copyProperties(user, newBeeMallUserVO);
+            //设置购物车中的数量
+            httpSession.setAttribute(Constants.MALL_USER_SESSION_KEY, newBeeMallUserVO);
+            return ServiceResultEnum.SUCCESS.getResult();
+        }
+        return ServiceResultEnum.LOGIN_ERROR.getResult();
+    }
+
+    @Override
+    public MallUserVO updateUserInfo(MallUser mallUser, HttpSession httpSession) {
+        MallUser user = mallUserMapper.selectByPrimaryKey(mallUser.getUserId());
+        if (user != null) {
+            user.setNickName(mallUser.getNickName());
+            user.setAddress(mallUser.getAddress());
+            user.setIntroduceSign(mallUser.getIntroduceSign());
+            if (mallUserMapper.updateByPrimaryKeySelective(user) > 0) {
+                MallUserVO newBeeMallUserVO = new MallUserVO();
+                user = mallUserMapper.selectByPrimaryKey(mallUser.getUserId());
+                BeanUtil.copyProperties(user, newBeeMallUserVO);
+                httpSession.setAttribute(Constants.MALL_USER_SESSION_KEY, newBeeMallUserVO);
+                return newBeeMallUserVO;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean lockUsers(Integer[] ids, int lockStatus) {
+        if (ids.length < 1) {
+            return false;
+        }
+        return mallUserMapper.lockUserBatch(ids, lockStatus) > 0;
+    }
+}
