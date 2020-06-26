321c208550f60f3c0bbe8ce87454b7366269ef66
GCZ <1635320153@qq.com>
2020-06-26 10:45:38 +0800

    购物车服务类

---
+++ src/main/java/edu/nwpu/market/service/NWPUMarketGoodsService.java
@@ -0,0 +1,66 @@
+package edu.nwpu.market.service;
+
+import edu.nwpu.market.entity.NWPUMarketGoods;
+import edu.nwpu.market.util.PageQueryUtil;
+import edu.nwpu.market.util.PageResult;
+
+
+import java.util.List;
+
+public interface NWPUMarketGoodsService {
+    /**
+     * 后台分页
+     *
+     * @param pageUtil
+     * @return
+     */
+    PageResult getNWPUMarketGoodsPage(PageQueryUtil pageUtil);
+
+    /**
+     * 添加商品
+     *
+     * @param goods
+     * @return
+     */
+    String saveNWPUMarketGoods(NWPUMarketGoods goods);
+
+    /**
+     * 批量新增商品数据
+     *
+     * @param NWPUMarketGoodsList
+     * @return
+     */
+    void batchSaveNWPUMarketGoods(List<NWPUMarketGoods> NWPUMarketGoodsList);
+
+    /**
+     * 修改商品信息
+     *
+     * @param goods
+     * @return
+     */
+    String updateNWPUMarketGoods(NWPUMarketGoods goods);
+
+    /**
+     * 获取商品详情
+     *
+     * @param id
+     * @return
+     */
+    NWPUMarketGoods getNWPUMarketGoodsById(Long id);
+
+    /**
+     * 批量修改销售状态(上架下架)
+     *
+     * @param ids
+     * @return
+     */
+    Boolean batchUpdateSellStatus(Long[] ids,int sellStatus);
+
+    /**
+     * 商品搜索
+     *
+     * @param pageUtil
+     * @return
+     */
+    PageResult searchNWPUMarketGoods(PageQueryUtil pageUtil);
+}
