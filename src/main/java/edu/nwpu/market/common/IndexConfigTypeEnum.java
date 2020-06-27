44d294325a0b7bd910774e3c99e7fa0e4be94432
cfz1885 <935562189@qq.com>
2020-06-27 17:34:13 +0800

    完成商品添加的页面跳转

--- src/main/java/edu/nwpu/market/common/IndexConfigTypeEnum.java
+++ src/main/java/edu/nwpu/market/common/IndexConfigTypeEnum.java
@@ -5,6 +5,8 @@ public enum IndexConfigTypeEnum {
     DEFAULT(0, "DEFAULT"),
     INDEX_SEARCH_HOTS(1, "INDEX_SEARCH_HOTS"),
     INDEX_SEARCH_DOWN_HOTS(2, "INDEX_SEARCH_DOWN_HOTS"),
+    INDEX_GOODS_HOT(3, "INDEX_GOODS_HOTS"),
+    INDEX_GOODS_NEW(4, "INDEX_GOODS_NEW"),
     INDEX_GOODS_RECOMMOND(5, "INDEX_GOODS_RECOMMOND");
 
     private int type;
@@ -41,4 +43,3 @@ public enum IndexConfigTypeEnum {
         this.name = name;
     }
 }
-
--- src/main/java/edu/nwpu/market/controller/admin/NWPUMarketGoodsIndexConfigController.java
+++ src/main/java/edu/nwpu/market/controller/admin/NWPUMarketGoodsIndexConfigController.java
@@ -1,22 +1,20 @@
 package edu.nwpu.market.controller.admin;
 
-import edu.nwpu.market.common.IndexConfigTypeEnum;
-import edu.nwpu.market.common.ServiceResultEnum;
-import edu.nwpu.market.entity.GoodsCategory;
-import edu.nwpu.market.entity.IndexConfig;
-import edu.nwpu.market.service.NWPUMarketIndexConfigService;
-import edu.nwpu.market.util.PageQueryUtil;
-import edu.nwpu.market.util.Result;
-import edu.nwpu.market.util.ResultGenerator;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-import java.util.Objects;
+import edu.nwpu.market.common.IndexConfigTypeEnum;
 
+
+
+@Controller
+@RequestMapping("/admin")
 public class NWPUMarketGoodsIndexConfigController {
-
+    @GetMapping("/indexConfigs")
+    public String indexConfigsPage(HttpServletRequest request, @RequestParam("configType") int configType) {
+        IndexConfigTypeEnum indexConfigTypeEnum = IndexConfigTypeEnum.getIndexConfigTypeEnumByType(configType);
+        request.setAttribute("path", indexConfigTypeEnum.getName());
+        request.setAttribute("configType", configType);
+        return "admin/nwpu_market_index_config";
+    }
 }
